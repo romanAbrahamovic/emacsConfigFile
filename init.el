6fa75b1bf52a24451953718e0f8221dbc98c2e42
bigi123 <ceo@mightcore.com>
2017-03-13 08:54:46 +0100

    Update init.el

--- init.el
+++ init.el
@@ -1,10 +1,3 @@
-; This is my super-poopy .emacs file.
-; I barely know how to program LISP, and I know
-; even less about ELISP.  So take everything in
-; this file with a grain of salt!
-;
-; - Casey
-
 ; Stop Emacs from losing undo information by
 ; setting very high limits for undo buffers
 (setq undo-limit 20000000)
@@ -27,10 +20,10 @@
 (setq enable-local-variables nil)
 (setq casey-font "outline-DejaVu Sans Mono")
 
-;(when casey-win32 
-;  (setq casey-makescript "build.bat")
-;  (setq casey-font "outline-Liberation Mono")
-;)
+(when casey-win32 
+ (setq casey-makescript "build.bat")
+  (setq casey-font "outline-Liberation Mono")
+)
 
 (when casey-aquamacs 
   (cua-mode 0) 
@@ -45,11 +38,11 @@
   (setq mac-command-key-is-meta t)
   (scroll-bar-mode nil)
   (setq mac-pass-command-to-system nil)
- ; (setq casey-makescript "./build.macosx")
+  (setq casey-makescript "./build.macosx")
 )
 
 (when casey-linux
-;  (setq casey-makescript "./build.linux")
+  (setq casey-makescript "./build.linux")
   (display-battery-mode 1)
 )
 
@@ -601,15 +594,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;; ADDED FOR PHP 
 
-;;; php-mode.el --- Major mode for editing PHP code
-
-;; Copyright (C) 1999, 2000, 2001, 2003, 2004 Turadg Aleahmad
-;;               2008 Aaron S. Hawley
-;;               2011, 2012, 2013, 2014 Eric James Michael Ritz
-
-;;; Author: Eric James Michael Ritz
-;;; URL: https://github.com/ejmr/php-mode
-;;; Version: 1.13.1
 
 (defconst php-mode-version-number "1.13.1"
   "PHP Mode version number.")
@@ -617,50 +601,6 @@
 (defconst php-mode-modified "2013-12-03"
   "PHP Mode build date.")
 
-;;; License
-
-;; This file is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License
-;; as published by the Free Software Foundation; either version 3
-;; of the License, or (at your option) any later version.
-
-;; This file is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this file; if not, write to the Free Software
-;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
-;; 02110-1301, USA.
-
-;;; Usage
-
-;; Put this file in your Emacs lisp path (eg. site-lisp) and add to
-;; your .emacs file:
-;;
-;;   (require 'php-mode)
-
-;; To use abbrev-mode, add lines like this:
-;;   (add-hook 'php-mode-hook
-;;     '(lambda () (define-abbrev php-mode-abbrev-table "ex" "extends")))
-
-;; To make php-mode compatible with html-mode, see http://php-mode.sf.net
-
-;; Many options available under Help:Customize
-;; Options specific to php-mode are in
-;;  Programming/Languages/Php
-;; Since it inherits much functionality from c-mode, look there too
-;;  Programming/Languages/C
-
-;;; Commentary:
-
-;; PHP mode is a major mode for editing PHP source code.  It's an
-;; extension of C mode; thus it inherits all C mode's navigation
-;; functionality.  But it colors according to the PHP grammar and
-;; indents according to the PEAR coding guidelines.  It also includes
-;; a couple handy IDE-type features such as documentation search and a
-;; source and class browser.
 
 ;;; Code:
 
@@ -2494,9 +2434,5 @@ The output will appear in the buffer *PHP*."
         ("\\.phpr"    . php-mode)
          ) auto-mode-alist))
 
-;;;###autoload 
-;(dolist (pattern '("\\.php[s345t]?\\'" "\\.phtml\\'" "\\.php[rm]?\\'"
-;  "Amkfile" "\\.amk$")) (add-to-list 'auto-mode-alist `(,pattern
-;  . php-mode) t)) (provide 'php-mode)
 
 ;;; php-mode.el ends here
